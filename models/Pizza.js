54c1acbfc88c1e37567acc8c211673b72b28495f
CP McClure <c.patrick.mcclure@gmail.com>
2022-08-31 19:41:21 -0400

    Update models/Todo.js to models/Pizza.js, update to pizza

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const TodoSchema = new mongoose.Schema({
-  todo: {
+const PizzaSchema = new mongoose.Schema({
+  pizza: {
     type: String,
     required: true,
   },
@@ -15,4 +15,4 @@ const TodoSchema = new mongoose.Schema({
   }
 })
 
-module.exports = mongoose.model('Todo', TodoSchema)
+module.exports = mongoose.model('Pizza', PizzaSchema)
